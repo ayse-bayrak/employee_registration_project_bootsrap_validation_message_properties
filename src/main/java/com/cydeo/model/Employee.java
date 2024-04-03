1248479796969e534ac3ef6d28c8c3ae6d93cb00
aysegunesbayrak <aysegunes.bayrak@gmail.com>
2024-04-03 11:21:49 +0300

    fixes validation

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -24,19 +24,21 @@ public class Employee {
    @NotBlank
    @Size(max=12, min=2)
     private String firstName;
+    @NotBlank
+    @Size(max=12, min=2)
     private String lastName;
     //Thymeleaf returns yyyy-MM-dd but LocalDate accepts mm-dd-yyyy
 
-// @NotNull
+    @NotNull
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
 
-//    @NotNull
-//    @Email
+    @NotNull
+    @Email
     private String email;
 
-//    @NotBlank
-//    @Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,}")
+    @NotBlank
+    @Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,}")
     private String password;
     private String address;
     private String address2;
